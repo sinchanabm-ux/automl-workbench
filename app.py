60fa29fa5cb3ccf1de93f47a42740cbff48f376c
sinchanabm-ux <sinchanabm459@gmail.com>
2026-08-28 02:31:15 +0530

    Retrieved columns, added dropdown to select target

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import os
+import pandas as pd
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'uploads'
@@ -14,7 +15,10 @@ def upload():
     file = request.files['csv_file']
     filepath = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
     file.save(filepath)
-    return f"File {file.filename} uploaded successfully!"
+    #converting CSV into DataFrame to obtain columns
+    df = pd.read_csv(filepath)
+    columns = df.columns.to_list()
+    return render_template('select_target.html', columns=columns)
 
 if __name__ == '__main__':
     app.run(debug = True)
